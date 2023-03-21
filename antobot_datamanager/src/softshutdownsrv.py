d3fba38f9d6701ba979deb00c20448fb304abe33
Zhuang Zhou <90620739+zhuang-antobot@users.noreply.github.com>
2023-03-21 15:18:36 +0000

    softshutdown without docker tested
    to be test again run as whole

--- antobot_datamanager/src/softshutdownsrv.py
+++ antobot_datamanager/src/softshutdownsrv.py
@@ -23,57 +23,25 @@
 #Contact:     zhuang.zhou@antobot.ai
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-import paramiko #the 3rd party tool to ssh
-from anto_msgs.srv import softshutdown, softshutdownResponse
+from antobot_msgs.srv import softshutdown, softshutdownResponse
 import os
 import rospy
 import time
 
-#host info to access from inside docker, can be removed if no docker in robot platform
-host = 'antobot-desktop'
-user = 'antobot'
-password = 'Antobot2021-'
  
-def ssh_exec_command(command): 
-    #ssh the host, send command passed from softshutdownprocess function
-    try: 
-        ssh_client = paramiko.SSHClient() 
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-        ssh_client.connect(host,22,user,password)       
-        print("command: " + command) #command passed from softshutdownprocess function
-        std_in, std_out, std_err = ssh_client.exec_command(command, get_pty=True) 
-        std_in.write(password + '\n')
-        for line in std_out: 
-            print(line.strip("\n"))
-        for line in std_err: 
-            print(line.strip("\n"))         
-        ssh_client.close() 
-    except Exception as e: 
-        print("error: " + str(e)) 
-
 def softshutdownprocess(req):
     return_msg = softshutdownResponse()
     print("soft shutdown service callback entered!")
     print("Sending softshutdown request to Aurix")
-    #ssh_exec_command("sudo shutdown +30") #shut down in 30 sec sudo shutdown -h now -h -t 30
-    time.sleep(4) #leave time to send /antobridge/soft_shutdown_req topic to antobridge, repeat 100 times
-     
+    time.sleep(4) #leave time to send /antobridge/soft_shutdown_req topic to antobridge, repeat 100 times    
     print("killing nodes now")
-    ##if want to kill all the nodes, it will take 15 sec
-    # nodes = os.popen("rosnode list").readlines()
-    # for i in range(len(nodes)):
-    #     nodes[i] = nodes[i].replace("\n","")
-    # for node in nodes:
-    #     if node != "/anto_bridge" and node != "/anto_supervisor" and node !="/soft_shutdown_server": #tbc if need this 
-    #         os.system("rosnode kill "+ node)
-    #os.system(" rosnode kill /anto_supervisor")
     os.system("rosnode kill /anto_bridge")
     return_msg.responseBool = True
     return_msg.responseString = "Success!"
     
     print("will shutdown in 2 sec ")
     time.sleep(2)
-    os.system("sudo shutdown -h now") #shutdown computer now
+    os.system("shutdown -h now") #shutdown computer now
     return return_msg
 
 def soft_shutdown_server():
