e1bbdafb9243cb32f893d7b327776c2f51d5fd05
DanielFreer-antobot <74193779+DanielFreer-antobot@users.noreply.github.com>
2023-05-11 14:12:05 +0100

    Cleaning up cameramanagerclient and softshutdownsrv

--- antobot_datamanager/src/softshutdownsrv.py
+++ antobot_datamanager/src/softshutdownsrv.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2019, ANTOBOT LTD.
+# Copyright (c) 2023, ANTOBOT LTD.
 # All rights reserved.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 #Description:   This is a python script to kill the nodes and shutdown host pc using ssh
-#Interface:
 #Inputs:      soft shutdown request [softshutdown] - soft shutdown client request send from Anto_supervisor      
 #Contact:     zhuang.zhou@antobot.ai
 
@@ -27,22 +14,17 @@ from antobot_msgs.srv import softShutdown, softShutdownResponse
 import os
 import rospy
 import time
-
-
  
 def softShutdownprocess(req):
     return_msg = softShutdownResponse()
-    print("soft shutdown service callback entered!")
-    print("Sending softshutdown request to Aurix")
-    time.sleep(4) #leave time to send /antobridge/soft_shutdown_req topic to antobridge, repeat 100 times    
-    print("killing nodes now")
+    time.sleep(4)   # leave time to send /antobridge/soft_shutdown_req topic to antobridge  
+    
     os.system("rosnode kill /anto_bridge")
     return_msg.responseBool = True
     return_msg.responseString = "Success!"
     
-    print("will shutdown in 2 sec ")
     time.sleep(2)
-    os.system("shutdown -h now") #shutdown computer now
+    os.system("shutdown -h now")  # shutdown computer now
     return return_msg
 
 def soft_shutdown_server():
