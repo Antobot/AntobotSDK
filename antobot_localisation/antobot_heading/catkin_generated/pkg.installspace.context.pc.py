ffb888f8060da9ed8cfcff33d6beb621ad5b7cac
Ash-antobot <aswathi.muralidharan@antobot.ai>
2023-05-23 10:43:07 +0100

    GPS changes and add GPS freq back in as
    Updating the gps_config file
    
    Updating the gps_config file

---
+++ antobot_localisation/antobot_heading/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;std_srvs;nav_msgs;sensor_msgs;tf2_geometry_msgs;tf2;geonav_transform".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "antobot_heading"
+PROJECT_SPACE_DIR = "/usr/local"
+PROJECT_VERSION = "0.0.0"
