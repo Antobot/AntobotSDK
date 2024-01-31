a6ca6e01fe1f44472472c72f2c58200cb6f2789f
DanielFreer-antobot <daniel.freer@antobot.ai>
2024-01-31 09:50:37 +0000

    Removing catkin_generated folders

--- antobot_localisation/antobot_heading/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/antobot/catkin_ws/src/AntobotSDK/antobot_localisation/antobot_heading/include".split(';') if "/home/antobot/catkin_ws/src/AntobotSDK/antobot_localisation/antobot_heading/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;std_srvs;nav_msgs;sensor_msgs;tf2_geometry_msgs;tf2;geonav_transform".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "antobot_heading"
-PROJECT_SPACE_DIR = "/home/antobot/catkin_ws/src/devel"
-PROJECT_VERSION = "0.0.0"
